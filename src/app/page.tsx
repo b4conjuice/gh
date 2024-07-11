06b4e2273eae0d09bb35a709a99d277659acb918
Daniel Lopez <d@niel.to>
2024-07-10 20:21:49 -0700

    fix links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { unstable_noStore as noStore } from 'next/cache'
 
 import Main from '@/app/_components/main'
 import fetcher from '@/lib/fetcher'
-import { LinkIcon } from '@heroicons/react/24/solid'
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/solid'
 
 // api:
 // https://docs.github.com/en/rest/repos?apiVersion=2022-11-28
@@ -35,22 +35,26 @@ export default async function Home() {
         <ul className='divide-y divide-cb-dusty-blue'>
           {repos.map(repo => (
             <li key={repo.id} className='flex items-center py-4 first:pt-0'>
-              <a
-                className='grow text-xl text-cb-pink hover:text-cb-pink/75'
-                href={repo.url}
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                {repo.name}
-              </a>
-              <a
-                className='text-cb-pink hover:text-cb-pink/75'
-                href={repo.homepage}
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                <LinkIcon className='h-6 w-6' />
-              </a>
+              <span className='grow'>
+                <a
+                  className='text-xl text-cb-pink hover:text-cb-pink/75'
+                  href={repo.url}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  {repo.name}
+                </a>
+              </span>
+              {repo.homepage && (
+                <a
+                  className='flex space-x-1 text-cb-pink hover:text-cb-pink/75'
+                  href={repo.homepage}
+                  target='_blank'
+                >
+                  <span>{repo.homepage}</span>
+                  <ArrowTopRightOnSquareIcon className='h-6 w-6' />
+                </a>
+              )}
             </li>
           ))}
         </ul>
